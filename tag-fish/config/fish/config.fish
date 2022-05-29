772d59c7ac9c877e8db55ca39e9aac56c0d6c99e
Allen.gitee <8676187+allengiteexyy@user.noreply.gitee.com>
2022-05-29 22:23:55 +0800

    update

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -7,13 +7,8 @@ scheme set tomorrow
 
 set proxy_host 127.0.0.1:8889
 set proxy_auth true
-set proxy_user allen
 
 if test -f /home/allen/.autojump/share/autojump/autojump.fish; . /home/allen/.autojump/share/autojump/autojump.fish; end
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/allen/.miniconda/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
 
 set TexMan /usr/local/texlive/2021/texmf-dist/doc/man
 set TexInfo /usr/local/texlive/2021/texmf-dist/doc/info
@@ -31,3 +26,9 @@ set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; se
 set CM_LAUNCHER rofi
 set CM_DIR /home/allen/.tmp
 set VAGRANT_DEFAULT_PROVIDER libvirt
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/allen/.miniconda/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
