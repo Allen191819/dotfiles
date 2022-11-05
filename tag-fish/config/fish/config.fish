92abeb727a91c372844f0d5f87268fcec9a0a4a1
Allen191819 <2020302191891@whu.edu.cn>
2022-11-06 00:40:39 +0800

    11-06-2022

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -12,22 +12,24 @@ if test -f /home/allen/.autojump/share/autojump/autojump.fish; . /home/allen/.au
 
 
 
-set TexMan /usr/local/texlive/2022/texmf-dist/doc/man
-set TexInfo /usr/local/texlive/2022/texmf-dist/doc/info
-set TexLive /usr/local/texlive/2022/bin/x86_64-linux
-set MANPATH /usr/local/man /usr/man /usr/share/man $MANPATH $TexMan
-set INFOPATH $INFOPATH $TexInfo
+set -x TexMan /usr/local/texlive/2022/texmf-dist/doc/man
+set -x TexInfo /usr/local/texlive/2022/texmf-dist/doc/info
+set -x TexLive /usr/local/texlive/2022/bin/x86_64-linux
+set -x MANPATH /usr/local/man /usr/man /usr/share/man $MANPATH $TexMan
+set -x INFOPATH $INFOPATH $TexInfo
 
-set PATH /home/allen/.local/bin /home/allen/go/bin /home/allen/.local/share/gem/ruby/3.0.0/bin /opt/cuda/bin /home/allen/.cargo/bin /home/allen/.stack/programs/x86_64-linux/ghc-tinfo6-9.0.2/bin $TexLive $PATH
-set GOPATH /home/allen/go/
-set GOPROXY "https://mirrors.aliyun.com/goproxy/"
+set -x PATH /home/allen/.local/bin /home/allen/go/bin /home/allen/.local/share/gem/ruby/3.0.0/bin /opt/cuda/bin /home/allen/.cargo/bin /home/allen/.stack/programs/x86_64-linux/ghc-tinfo6-9.0.2/bin /home/allen/Android/Sdk/build-tools/30.0.3 $TexLive $PATH
+set -x GOPATH /home/allen/go/
+set -x GOPROXY "https://mirrors.aliyun.com/goproxy/"
 
+set -x REPO_DIR "/home/allen/Workplace/sp21-s198"
+set -x SNAPS_DIR "/home/allen/Workplace/snaps-sp21-s198"
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/allen/.ghcup/bin $PATH # ghcup-env
 
-set CM_LAUNCHER rofi
-set CM_DIR /home/allen/.tmp
-set VAGRANT_DEFAULT_PROVIDER libvirt
+set -x CM_LAUNCHER rofi
+set -x CM_DIR /home/allen/.tmp
+set -x VAGRANT_DEFAULT_PROVIDER libvirt
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
