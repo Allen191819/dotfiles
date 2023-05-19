26af7fd2d745a93b4e78e5f57f1d249dfdd120b3
Allen191819 <2020302191891@whu.edu.cn>
2023-05-20 01:14:04 +0800

    05-20-2023

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -33,8 +33,11 @@ set -x VAGRANT_DEFAULT_PROVIDER libvirt
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/allen/.miniconda/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
+#
+set -gx EDITOR "nvim"
 set fzf_fd_opts --hidden --exclude=.git
 set fzf_preview_dir_cmd exa --all --color=always --icons
+set fzf_dir_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 
 set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
 set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
@@ -43,5 +46,8 @@ set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; se
 
 set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
+set -x CM_LAUNCHER dmenu
+
 # opam configuration
 source /home/allen/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+scheme set dracula
